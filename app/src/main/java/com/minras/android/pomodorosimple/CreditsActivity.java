f31b44727819f0cb8c5482344cb32543497062b8
Minras <andrey.shchurkov@gmail.com>
2016-03-09 22:51:49 +0100

    #6 Added credits page

--- app/src/main/java/com/minras/android/pomodorosimple/CreditsActivity.java
+++ app/src/main/java/com/minras/android/pomodorosimple/CreditsActivity.java
@@ -1,7 +1,10 @@
 package com.minras.android.pomodorosimple;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.widget.TextView;
 
 public class CreditsActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class CreditsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        // now format the texts
+        TextView creditsTextIcon = (TextView) findViewById(R.id.credits_text_icon);
+        creditsTextIcon.setText(
+                Html.fromHtml(
+                        getResources().getString(R.string.credits_text_icon)));
+
     }
 }
